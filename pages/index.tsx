274a1295af9e57e51c054d1deccd88d815ef1251
nelsonfrz <117679845+nelsonfrz@users.noreply.github.com>
2023-01-28 17:40:33 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,27 +22,27 @@ export default function Home({ articles, projects }: HomeProps) {
     <Head>
       <title>@nelsonfrz - Home</title>
       <meta name="title" content="@nelsonfrz - Home" />
-      <meta name="description" content="I&apos;m a Fullstack Web Developer located in Germany. Ocassionally I write articles about Software Engineering, Web Development and anything else." />
+      <meta name="description" content="I&apos;m a Software Engineer located in Germany. Ocassionally I write articles about Software Engineering, Web Development and anything else." />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://nelsonfrz.vercel.app/" />
       <meta property="og:title" content="@nelsonfrz - Home" /> 
-      <meta property="og:description" content="I&apos;m a 15 year old boy located in Germany with a passion for Web Development. Ocassionally I write articles about Software Engineering, Web Development and anything else." />
+      <meta property="og:description" content="I&apos;m a 15 year old boy located in Germany with a passion for Software Engineering. Ocassionally I write articles about Software Engineering, Web Development and anything else." />
       <meta property="og:image" content="https://i.postimg.cc/ZYVMxRQF/image.png" />
 
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content="https://nelsonfrz.vercel.app/"/>
       <meta property="twitter:title" content="@nelsonfrz - Home"/>
-      <meta property="twitter:description" content="I&apos;m a 15 year old boy located in Germany with a passion for Web Development. Ocassionally I write articles about Software Engineering, Web Development and anything else."/>
+      <meta property="twitter:description" content="I&apos;m a 15 year old boy located in Germany with a passion for Software Engineering. Ocassionally I write articles about Software Engineering, Web Development and anything else."/>
       <meta property="twitter:image" content="https://i.postimg.cc/ZYVMxRQF/image.png"></meta>
     </Head>
     <Layout>
       <section className="text-center my-[200px]">
         <h1 className="text-[100px] font-black leading-none p-2 max-lg:text-[80px] max-sm:text-[48px]">
           <span className="text-gradient-black">Hello! I&apos;m a</span> <br />
-          <span className="text-gradient-purple-animated">Fullstack Web Developer</span> <br />
+          <span className="text-gradient-purple-animated">Software Engineer</span> <br />
         </h1>
-        <p className="max-w-2xl mx-auto text-[16px] font-normal text-[#666666]">I&apos;m a 15 year old boy located in Germany with a passion for Web Development. Ocassionally I write articles about Software Engineering, Web Development and anything else.</p>
+        <p className="max-w-2xl mx-auto text-[16px] font-normal text-[#666666]">I&apos;m a 15 year old boy located in Germany with a passion for Software Engineering. Ocassionally I write articles about Software Engineering, Web Development and anything else.</p>
         <LinkButton color="purple" className="mt-4 mr-2" url="/articles">Read Articles</LinkButton>
         <LinkButton color="white" className="mt-4 ml-2" url="/projects">View Projects</LinkButton>
       </section>
